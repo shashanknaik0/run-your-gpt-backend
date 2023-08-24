fb1dbc045f2ba5e65b7ee802064681b25ba05a33
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-08-24 10:16:48 +0530

    Add. cors in setting

--- run_your_gpt/settings.py
+++ run_your_gpt/settings.py
@@ -121,3 +121,21 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+    'http://127.0.0.1:3000',
+]
+
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
+
+CORS_ALLOW_METHODS = [
+'DELETE',
+'GET',
+'OPTIONS',
+'PATCH',
+'POST',
+'PUT',
+]
