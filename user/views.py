7e3f92e3f017e220dae32b3aabd7e32b385ea5fc
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-08-24 10:30:52 +0530

    Add. signup logic

--- user/views.py
+++ user/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+import json
+from django.http import HttpResponse
+from django.contrib.auth.models import User
 
 # Create your views here.
+def Signup(req):
+    if req.method== "POST":
+        data = json.loads(req.body)
+        fname = data["fname"]
+        lname = data["lname"]
+        uname = data["uname"]
+        pwd = data["pwd"]
+        email = data["email"]
+        
+        myuser = User(username=uname,email=email,password=pwd)
+        myuser.first_name = fname
+        myuser.last_name = lname
+
+        myuser.save()
+
+        return HttpResponse("signin success")
+    else:
+        return HttpResponse("signin failed", status=505)
