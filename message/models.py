cd257b9661f95bc0d6f104e22483a18bd3fe5d55
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-08-24 10:59:31 +0530

    Add. message model

--- message/models.py
+++ message/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    userId = models.ForeignKey(to=User,on_delete=models.CASCADE)
+    userInput = models.CharField(max_length=255)
+    response = models.CharField(max_length=255,null=False)
+    createdAt = models.DateTimeField(auto_now_add=True)
