55c9f98faee95ea3f2ef4769a3f3e099423fcce5
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-08-24 11:02:43 +0530

    Add. message to admin site

--- message/admin.py
+++ message/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Message
 
 # Register your models here.
+class MessageAdmin(admin.ModelAdmin):
+    list_display=["id", "user_id", "user_input", "response", "created_at"]
+
+admin.site.register(Message, MessageAdmin)
--- message/models.py
+++ message/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Message(models.Model):
     id = models.AutoField(primary_key=True)
-    userId = models.ForeignKey(to=User,on_delete=models.CASCADE)
-    userInput = models.CharField(max_length=255)
+    user_id = models.ForeignKey(to=User,on_delete=models.CASCADE)
+    user_input = models.CharField(max_length=255)
     response = models.CharField(max_length=255,null=False)
-    createdAt = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
