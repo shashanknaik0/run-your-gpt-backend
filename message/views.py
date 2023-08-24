18e0632f944f62be9a685c1a643f4ffbece7881e
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-08-24 11:29:15 +0530

    Add. get user message logic

--- message/views.py
+++ message/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from .models import Message
 
 # Create your views here.
+@login_required
+def message(request):
+    if request.method == "GET":
+        userId = request.user.id
+        items = Message.objects.all().filter(user_id = userId).order_by('created_at')
+        result = []
+        for x in items:
+            result.append({"id":x.id,"user_input":x.user_input,"response":x.response, "created_at":x.created_at})
+
+        return JsonResponse(result,safe=False)
--- run_your_gpt/settings.py
+++ run_your_gpt/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'user',
+    'message',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
