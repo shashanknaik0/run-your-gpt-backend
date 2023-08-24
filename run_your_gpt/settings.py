490576acbc5b7c794aea2b78c9ade5869628fc82
Shashank Naik <shashankkrishnanaik@gmail.com>
2023-08-24 14:32:30 +0530

    Add. cors middleware

--- run_your_gpt/settings.py
+++ run_your_gpt/settings.py
@@ -35,9 +35,9 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'corsheaders',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'user',
     'message',
 ]
@@ -49,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'run_your_gpt.urls'
